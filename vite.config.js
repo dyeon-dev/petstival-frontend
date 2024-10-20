99b9a830195e85a3acca854a99eb53dad10e19f6
shinhm1 <shinhm1@naver.com>
2024-10-21 01:24:22 +0900

    chore: Edge Function 세팅, 로컬 세팅, 배포 테스트 및 호출 테스트 완료

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'http://127.0.0.1:54321/functions/v1', // edge function local 주소
+        target: 'https://hfnchwvpqruwmlehusbs.supabase.co/functions/v1', // edge function 배포 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
