6dc01c32df5370e3e6be6bc3b729dca16acbc585
kimdayeon37 <kdy37912@naver.com>
2024-10-21 16:19:46 +0900

    feat: 결제 요청 성공 시 UI 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CheckoutPage } from './PaymentsPage/CheckoutPage';
+import { SuccessPage } from './PaymentsPage/SuccessPage';
 
 function App() {
   return (
     <div>
-      <CheckoutPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<CheckoutPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/PaymentsPage/CheckoutPage.jsx
+++ src/PaymentsPage/CheckoutPage.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
-import { loadTossPayments, ANONYMOUS } from '@tosspayments/tosspayments-sdk';
+import { useEffect, useRef, useState } from "react";
+import { loadTossPayments, ANONYMOUS } from "@tosspayments/tosspayments-sdk";
 import { v4 as uuidv4 } from 'uuid';
 import './style.css';
 
 const generateRandomString = () => window.btoa(Math.random()).slice(0, 20);
-const clientKey = 'test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm';
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
 
 export function CheckoutPage() {
   const [ready, setReady] = useState(false);
   const [widgets, setWidgets] = useState(null);
 
-  // ------ 주문의 결제 금액 설정 ------
+   // ------ 주문의 결제 금액 설정 ------
   const [amount, setAmount] = useState({
-    currency: 'KRW',
+    currency: "KRW",
     value: 50000,
   });
 
   useEffect(() => {
     async function fetchPaymentWidgets() {
       const tossPayments = await loadTossPayments(clientKey); // 클라이언트 키로 TossPayments() 초기화 메서드를 호출하면 토스페이먼츠 객체가 생성된다.
-      const customerKey = uuidv4();
+      const customerKey = uuidv4()
       const widgets = tossPayments.widgets({ customerKey: customerKey });
       // 비회원 결제
       // const widgets = tossPayments.widgets({ customerKey: ANONYMOUS });
@@ -47,19 +47,19 @@ export function CheckoutPage() {
          * @docs https://docs.tosspayments.com/sdk/v2/js#widgetsrenderpaymentmethods
          */
         widgets.renderPaymentMethods({
-          selector: '#payment-method',
+          selector: "#payment-method",
           // 렌더링하고 싶은 결제 UI의 variantKey
           // 결제 수단 및 스타일이 다른 멀티 UI를 직접 만들고 싶다면 계약이 필요해요.
           // @docs https://docs.tosspayments.com/guides/v2/payment-widget/admin#새로운-결제-ui-추가하기
-          variantKey: 'DEFAULT',
+          variantKey: "DEFAULT",
         }),
         /**
          * 약관을 렌더링합니다.
          * @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
          */
         widgets.renderAgreement({
-          selector: '#agreement',
-          variantKey: 'AGREEMENT',
+          selector: "#agreement",
+          variantKey: "AGREEMENT",
         }),
       ]);
 
@@ -69,17 +69,42 @@ export function CheckoutPage() {
     renderPaymentWidgets();
   }, [widgets]);
 
+
   return (
     <div className="wrapper w-100">
       <div className="max-w-540 w-100">
-        {/* <!-- 결제 UI --> */}
+      {/* <!-- 결제 UI --> */}
         <div id="payment-method" className="w-100" />
         {/* <!-- 이용약관 UI --> */}
         <div id="agreement" className="w-100" />
         <div className="btn-wrapper w-100">
-          {/* // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------ */}
-          {/* <!-- 결제하기 버튼 --> */}
-          <button className="btn primary w-100">결제하기</button>
+        {/* // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------ */}
+        {/* <!-- 결제하기 버튼 --> */}
+          <button
+            className="btn primary w-100"
+            onClick={async () => {
+              try {
+                /**
+                 * 결제 요청
+                 * 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+                 * 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
+                 * @docs https://docs.tosspayments.com/sdk/v2/js#widgetsrequestpayment
+                 */
+                await widgets?.requestPayment({
+                  orderId: generateRandomString(),
+                  orderName: "토스 티셔츠 외 2건",
+                  customerName: "김토스",
+                  customerEmail: "customer123@gmail.com",
+                   successUrl: window.location.origin + "/success" + window.location.search,
+                   failUrl: window.location.origin + "/fail" + window.location.search
+                });
+              } catch (error) {
+                // TODO: 에러 처리
+              }
+            }}
+          >
+            결제하기
+          </button>
         </div>
       </div>
     </div>
