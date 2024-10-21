020d81b2f9af95ccf7834fd3412c611cc46e82cc
shinhm1 <shinhm1@naver.com>
2024-10-22 03:10:03 +0900

    chore: RouterDef 추가 및 Pages 폴더 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import AppPages from './routes/AppPages';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div>hi</div>
-    </>
-  );
+  return <AppPages />;
 }
 
 export default App;
---
+++ src/routes/RouteDef.jsx
@@ -0,0 +1,19 @@
+import LoginPage from '../pages/LoginPage';
+
+// 페이지 URL 및 렌더링할 페이지 컴포넌트 정보를 저장하는 객체
+export const Screens = {
+  Login: {
+    path: '/login', // url 경로 지정
+    element: <LoginPage />, // 렌더링할 페이지 컴포넌트 지정
+  },
+
+  // 페이지를 추가 시 아래에 새로운 페이지 객체 작성
+  // Survey: {
+  //   path: '/survey',
+  //   element: null,
+  // },
+};
+
+export const RouteDef = {
+  ...Screens,
+};
