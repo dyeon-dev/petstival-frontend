d6bc4bcff8ddb438fe66c84a566bce0bac714472
kimdayeon37 <kdy37912@naver.com>
2024-10-23 15:00:35 +0900

    style: mypage 나의 프로필

--- src/routes/RouteDef.jsx
+++ src/routes/RouteDef.jsx
@@ -1,7 +1,8 @@
 import LoginPage from '../pages/LoginPage';
-import CheckoutPage from "../PaymentsPage/CheckoutPage";
-import SuccessPage from "../PaymentsPage/SuccessPage";
-import FailPage from "../PaymentsPage/FailPage";
+import CheckoutPage from "../pages/PaymentsPage/CheckoutPage";
+import SuccessPage from "../pages/PaymentsPage/SuccessPage";
+import FailPage from "../pages/PaymentsPage/FailPage";
+import MyPage from "../pages/MyPage/MyPage";
 
 // 페이지 URL 및 렌더링할 페이지 컴포넌트 정보를 저장하는 객체
 export const Screens = {
@@ -20,6 +21,10 @@ export const Screens = {
   Fail: {
     path: '/fail', // url 경로 지정
     element: <FailPage />, // 렌더링할 페이지 컴포넌트 지정
+  },
+  MyPage: {
+    path: '/mypage', // url 경로 지정
+    element: <MyPage />, // 렌더링할 페이지 컴포넌트 지정
   },
   // 페이지를 추가 시 아래에 새로운 페이지 객체 작성
   // Survey: {
