e63fa47f3e55502abd8d4228bad8e54e8af36ca3
kimdayeon37 <kdy37912@naver.com>
2024-10-24 01:00:38 +0900

    style: 주문 아이템 list 추가

--- src/components/Mypage/OrderPage/OrderList.jsx
+++ src/components/Mypage/OrderPage/OrderList.jsx
@@ -4,64 +4,58 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
-import image1 from '../../../assets/info_image.png';
 
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
-
-
-export default function OrderList() {
+export default function OrderList({ item }) {
   return (
-    <Paper
-      sx={(theme) => ({
-        p: 2,
-        margin: 'auto',
-        maxWidth: 500,
-        flexGrow: 1,
-        backgroundColor: '#fff',
-        ...theme.applyStyles('dark', {
-          backgroundColor: '#1A2027',
-        }),
-      })}
-    >
-      <Grid item container spacing={2} sx={{ color: 'text.secondary', marginLeft: '10px' }}>
-        <Typography variant="body2" component="div">
-          2024.10.23 01:17 &nbsp;
-        </Typography>
-        <Typography variant="body2" component="div">
-          결제 완료
-        </Typography>
-      </Grid>
-
-      <Grid container spacing={2}>
-        <Grid item>
-          <ButtonBase sx={{ width: 100, height: 100 }}>
-            <Img alt="info_image" src={image1} />
-          </ButtonBase>
+    <>
+      <Paper
+        sx={(theme) => ({
+          p: 2,
+          margin: 'auto',
+          marginBottom: '15px',
+          maxWidth: 500,
+          flexGrow: 1,
+          backgroundColor: '#fff',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#1A2027',
+          }),
+        })}
+      >
+        <Grid item container spacing={2} sx={{ color: 'text.secondary', marginLeft: '10px' }}>
+          <Typography variant="body2" component="div">
+            {item.dateTime} &nbsp;
+          </Typography>
+          <Typography variant="body2" component="div">
+            {item.status}
+          </Typography>
         </Grid>
 
-        <Grid item xs={12} sm container sx={{  marginTop: '10px' }}>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-              <Typography gutterBottom variant="subtitle1" component="div" sx={{ cursor: 'pointer' }}>
-                강아지와 함께하는 피크닉 세트
-              </Typography>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase sx={{ width: 100, height: 100 }}>
+              <img alt={item.title} src={item.img} />
+            </ButtonBase>
+          </Grid>
 
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                1개
-              </Typography>
+          <Grid item xs={12} sm container sx={{ marginTop: '10px' }}>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" component="div" sx={{ cursor: 'pointer', fontWeight: 'bold' }}>
+                  {item.title}
+                </Typography>
 
-              <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                39,800원
-              </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  {item.amount}개
+                </Typography>
+
+                <Typography sx={{ cursor: 'pointer', fontWeight: 'bold' }}>
+                  {item.price}원
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </>
   );
 }
--- src/pages/MyPage/OrderPage/OrderPage.jsx
+++ src/pages/MyPage/OrderPage/OrderPage.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import OrderList from "../../../components/Mypage/OrderPage/OrderList";
-import Navbar from "../../../components/Navbar/Navbar";
+import OrderList from '../../../components/Mypage/OrderPage/OrderList';
+import Navbar from '../../../components/Navbar/Navbar';
+import image1 from '../../../assets/info_image.png';
 
 const Wrapper = styled.section`
-    margin-left: 24px;
-    margin-right: 24px;
+  margin-left: 24px;
+  margin-right: 24px;
 `;
+
+const itemData = [
+  {
+    img: image1,
+    title: '강아지와 함께하는 피크닉 세트',
+    amount: '1',
+    price: 39800,
+    dateTime: '2024.10.23 01:17',
+    status: '결제 완료',
+  },
+  {
+    img: image1,
+    title: '강아지와 함께하는 물놀이 세트',
+    amount: '1',
+    price: 39800,
+    dateTime: '2024.10.23 01:17',
+    status: '결제 완료',
+  },
+];
 
 function OrderPage() {
   return (
     <>
       <Wrapper>
-        <OrderList />
+        {itemData.map((item, index) => (
+          <OrderList key={index} item={item} />
+        ))}
       </Wrapper>
       <Navbar />
     </>
