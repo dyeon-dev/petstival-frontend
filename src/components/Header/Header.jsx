6a98de74247eddb4f994013472a6a90b85d8da56
shinhm1 <shinhm1@naver.com>
2024-10-24 11:20:57 +0900

    chore: svgr 플러그인 설치 및 설정 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css';
-import Logo from "./Logo";
+import Logo from '../../assets/logo/logo.svg?react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
