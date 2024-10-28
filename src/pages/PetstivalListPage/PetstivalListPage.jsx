4c6a101983a98cbe0b1bb39ac3bfdbcc36c12e64
kimdayeon37 <kdy37912@naver.com>
2024-10-28 11:36:51 +0900

    style: paper css 적용

--- src/pages/PetstivalListPage/PetstivalListPage.jsx
+++ src/pages/PetstivalListPage/PetstivalListPage.jsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
-
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
@@ -19,10 +20,30 @@ export default function PetstivalListPage() {
       <Wrapper>
         <ImageList cols={1}>
           {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`} src={`${item.img}?w=248&fit=crop&auto=format`} alt={item.title} loading="lazy" />
-              <ImageListItemBar title={item.title} subtitle={<span>by: {item.author}</span>} position="below" />
-            </ImageListItem>
+            <Paper
+              sx={(theme) => ({
+                p: 2,
+                marginBottom: '15px',
+                flexGrow: 1,
+                backgroundColor: '#fff',
+                borderRadius: '8px',
+                boxShadow: '0px 0px 8px 0px rgba(51, 51, 51, 0.08)',
+              })}
+            >
+              <ImageListItem key={item.img}>
+                <img
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar 
+                  title={<Typography variant="h6">{item.title}</Typography>} 
+                  subtitle={<Typography sx={{ marginTop: '4px' }}>{item.date}</Typography>}
+                  position="below" 
+                />
+              </ImageListItem>
+            </Paper>
           ))}
         </ImageList>
       </Wrapper>
@@ -35,61 +56,61 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
-    author: '@bkristastucchio',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
-    author: '@rollelflex_graphy726',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
-    author: '@helloimnik',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
-    author: '@nolanissac',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
-    author: '@hjrc33',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
-    author: '@arwinneil',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
     title: 'Basketball',
-    author: '@tjdragotta',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
     title: 'Fern',
-    author: '@katie_wasserman',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
     title: 'Mushrooms',
-    author: '@silverdalex',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
     title: 'Tomato basil',
-    author: '@shelleypauls',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
     title: 'Sea star',
-    author: '@peterlaster',
+    date: '2023.10.10 - 2023.10.13',
   },
   {
     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
     title: 'Bike',
-    author: '@southside_customs',
+    date: '2023.10.10 - 2023.10.13',
   },
 ];
