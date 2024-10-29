3b11f42b02848e6f52cdaed9e33ee4313af75dfb
kimdayeon37 <kdy37912@naver.com>
2024-10-29 15:01:46 +0900

    feat: header, navbar 추가

--- src/pages/ShopPage/ProductDetailPage.jsx
+++ src/pages/ShopPage/ProductDetailPage.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useProductStore } from '../../stores/useProductStore';
 import ItemSelectContainer from '../../components/ProductDetail/ItemSelectContainer';
-import { Button } from '../../stories/Button';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
 import styles from './ProductDetailPage.module.css';
 
 const ProductDetailPage = () => {
@@ -23,10 +24,12 @@ const ProductDetailPage = () => {
   };
 
   return (
+    <>
+    <Header />
     <div className={styles.detailContainer}>
       {/* 제품 이미지 - 이미지 스토리지에 넣은 URL 사용 */}
       <img
-        src="https://hfnchwvpqruwmlehusbs.supabase.co/storage/v1/object/sign/product-img/test-img.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0LWltZy90ZXN0LWltZy5wbmciLCJpYXQiOjE3Mjk4MTU1MTYsImV4cCI6MTczMjQwNzUxNn0.gMo6v5s3Fm4KFfooLtRGbJYPqxKk8cxIToUuCk4kMEg&t=2024-10-25T00%3A18%3A36.276Z"
+        src="https://hfnchwvpqruwmlehusbs.supabase.co/storage/v1/object/public/test/test-img.png?t=2024-10-29T04%3A54%3A48.164Z"
         alt={product.title}
         className={styles.productDetailImage}
       />
@@ -44,10 +47,12 @@ const ProductDetailPage = () => {
       <ItemSelectContainer price={product.price} />
       {/* 버튼 영역 */}
       <div className={styles.buttonWrapper}>
-        <Button variant="cartButton" text="장바구니에 넣기" onClick={handleAddToCart} />
-        <Button variant="buyButton" text="구매하기" onClick={handleBuyNow} />
+        <button onClick={handleAddToCart}>장바구니</button>
+        <button onClick={handleBuyNow}>구매하기</button>
       </div>
     </div>
+    <Navbar selectedMenu="Shop" />
+    </>
   );
 };
 
