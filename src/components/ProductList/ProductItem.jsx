05e60e6a3ef5f9744dab9b402579a56ce6fade30
kimdayeon37 <kdy37912@naver.com>
2024-10-29 14:33:25 +0900

    fix: 링크 스코프 변경

--- src/components/ProductList/ProductItem.jsx
+++ src/components/ProductList/ProductItem.jsx
@@ -6,13 +6,15 @@ const ProductItem = ({ id, title, price, imageSrc }) => {
   // 상품 제목이 길어서 제목을 조정할 수 있게 수정
   const truncatedTitle = title.length > 12 ? `${title.slice(0, 10)}...` : title;
   return (
-    <div className={styles.item}>
-      <img src={imageSrc} alt={title} className={styles.productImage} />
-      <Link to={`/products/${id}`} className={styles.productTitle}>
-        <h3>{truncatedTitle}</h3>
-      </Link>
-      <p className={styles.productPrice}>{price}원</p>
-    </div>
+    <Link to={`/products/${id}`}>
+      <div className={styles.item}>
+        <img src={imageSrc} alt={title} className={styles.productImage} />
+        <div className={styles.productTitle}>
+          <h3>{truncatedTitle}</h3>
+        </div>
+        <p className={styles.productPrice}>{price}원</p>
+      </div>
+    </Link>
   );
 };
 
