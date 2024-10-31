d7a4314247c3874af622103af198967e9d1b454d
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2024-10-31 17:23:05 +0900

    네비바 버튼클릭시 유효성 검사

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,29 +5,38 @@ import PetIcon from '../../assets/icons/pet.svg?react';
 import ShopIcon from '../../assets/icons/shop.svg?react';
 import UserIcon from '../../assets/icons/user.svg?react';
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from '../../stores/useAuthStore';
 
 export default function Navbar(props) {
   const navigate = useNavigate();
+  const user = useAuthStore((state) => state.user);
+
+  const handleNavigation = (path) => {
+    if (user || path === '/home' || path === '/products') {
+      navigate(path);
+    } else {
+      navigate('/login'); // 유저 정보가 null일 때 로그인 페이지로 이동
+    }
+  };
+
   return (
-    <>
-      <div className={styles.NavbarLayout}>
-        <div onClick={() => navigate('/home')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Home' ? `${styles.NavBarBtnSelected}` : ''}`}>
-          <HomeIcon fill={props.selectedMenu === 'Home' ? `var(--primary-default)` : `var(--gray-40)`} />
-          <div>홈</div>
-        </div>
-        <div onClick={() => navigate('/products')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Shop' ? `${styles.NavBarBtnSelected}` : ''}`}>
-          <ShopIcon fill={props.selectedMenu === 'Shop' ? `var(--primary-default)` : `var(--gray-40)`} />
-          <div>쇼핑</div>
-        </div>
-        <div onClick={() => navigate('/pet')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Pet' ? `${styles.NavBarBtnSelected}` : ''}`}>
-          <PetIcon fill={props.selectedMenu === 'Pet' ? `var(--primary-default)` : `var(--gray-40)`} />
-          <div>반려견</div>
-        </div>
-        <div onClick={() => navigate('/mypage')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'MyPage' ? `${styles.NavBarBtnSelected}` : ''}`}>
-          <UserIcon fill={props.selectedMenu === 'MyPage' ? `var(--primary-default)` : `var(--gray-40)`} />
-          <div>내 정보</div>
-        </div>
+    <div className={styles.NavbarLayout}>
+      <div onClick={() => handleNavigation('/home')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Home' ? `${styles.NavBarBtnSelected}` : ''}`}>
+        <HomeIcon fill={props.selectedMenu === 'Home' ? `var(--primary-default)` : `var(--gray-40)`} />
+        <div>홈</div>
       </div>
-    </>
+      <div onClick={() => handleNavigation('/products')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Shop' ? `${styles.NavBarBtnSelected}` : ''}`}>
+        <ShopIcon fill={props.selectedMenu === 'Shop' ? `var(--primary-default)` : `var(--gray-40)`} />
+        <div>쇼핑</div>
+      </div>
+      <div onClick={() => handleNavigation('/pet')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'Pet' ? `${styles.NavBarBtnSelected}` : ''}`}>
+        <PetIcon fill={props.selectedMenu === 'Pet' ? `var(--primary-default)` : `var(--gray-40)`} />
+        <div>반려견</div>
+      </div>
+      <div onClick={() => handleNavigation('/mypage')} className={`${styles.NavBarBtn} ${props.selectedMenu === 'MyPage' ? `${styles.NavBarBtnSelected}` : ''}`}>
+        <UserIcon fill={props.selectedMenu === 'MyPage' ? `var(--primary-default)` : `var(--gray-40)`} />
+        <div>내 정보</div>
+      </div>
+    </div>
   );
 }
