c8a1bf8913c8971a81227f13006f1b17ef085a02
kimyoungdong <kimyoungdong>
2024-11-01 14:11:08 +0900

    feat : 메인페이지 카테고리 클릭시 해당 카테고리의 쇼핑 탭으로 가도록 구현 / 주문하기 페이지 도로명주소 검색 활용하여 배달 정보 변경

--- src/components/PaymentsPage/DeliveryInfo.jsx
+++ src/components/PaymentsPage/DeliveryInfo.jsx
@@ -2,34 +2,59 @@ import { useEffect, useState } from 'react';
 import useDeliveryStore from '../../stores/useDeliveryStore';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Plus from '../../assets/icons/plus_info.svg?react';
-import ButtonMedium from '../../components/Common/Button/ButtonMedium';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function DeliveryInfo() {
   const [save, setSave] = useState(true);
   const { name, number, address, detailAddress, setName, setNumber, setAddress, setDetailAddress } = useDeliveryStore();
 
-  // 모든 필드가 채워졌는지 확인
   const isFormComplete = name && number && address && detailAddress;
 
   const handleInfo = () => {
     setSave(false);
   };
 
+  const handleAddressSearch = () => {
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        let addr = ''; // 주소 변수
+        let extraAddr = ''; // 참고항목 변수
+
+        if (data.userSelectedType === 'R') {
+          addr = data.roadAddress;
+        } else {
+          addr = data.jibunAddress;
+        }
+
+        if (data.userSelectedType === 'R') {
+          if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+            extraAddr += data.bname;
+          }
+          if (data.buildingName !== '' && data.apartment === 'Y') {
+            extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+          }
+          if (extraAddr !== '') {
+            extraAddr = ` (${extraAddr})`;
+          }
+        }
+
+        setAddress(`${addr} ${extraAddr}`);
+      },
+    }).open();
+  };
+
   return (
     <>
       <h3>배송지 정보</h3>
       <Paper
-        sx={(theme) => ({
+        sx={{
           p: 2,
           margin: 'auto',
           marginBottom: '15px',
@@ -38,7 +63,7 @@ export default function DeliveryInfo() {
           flexGrow: 1,
           backgroundColor: '#fff',
           boxShadow: '0px 0px 8px 0px rgba(51, 51, 51, 0.08)',
-        })}
+        }}
       >
         {save ? (
           <Accordion sx={{ borderRadius: '8px', backgroundColor: 'var(--primary-bright)' }}>
@@ -47,16 +72,43 @@ export default function DeliveryInfo() {
               <Typography>배송지 정보 추가하기</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <TextField sx={{ mb: 1 }} fullWidth required id="outlined-required" label="이름을 입력해주세요" onChange={(e) => setName(e.target.value)} />
-              <TextField sx={{ mb: 1 }} fullWidth required id="outlined-required" label="전화번호를 입력해주세요" onChange={(e) => setNumber(e.target.value)} />
               <TextField
                 sx={{ mb: 1 }}
                 fullWidth
                 required
                 id="outlined-required"
-                label="도로명 주소를 입력해주세요"
-                onChange={(e) => setAddress(e.target.value)}
+                label="이름을 입력해주세요"
+                onChange={(e) => setName(e.target.value)}
               />
+              <TextField
+                sx={{ mb: 1 }}
+                fullWidth
+                required
+                id="outlined-required"
+                label="전화번호를 입력해주세요"
+                onChange={(e) => setNumber(e.target.value)}
+              />
+
+              <Grid container spacing={1} sx={{ mb: 1 }}>
+                <Grid item xs={9}>
+                  <TextField
+                    fullWidth
+                    required
+                    id="outlined-required"
+                    label="도로명 주소를 입력해주세요"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <Button variant="outlined" onClick={handleAddressSearch} sx={{ height: '100%' }}>
+                    주소 
+                    <br></br>
+                    검색
+                  </Button>
+                </Grid>
+              </Grid>
+
               <TextField
                 sx={{ mb: 1 }}
                 fullWidth
@@ -67,8 +119,14 @@ export default function DeliveryInfo() {
               />
 
               <Grid sx={{ mt: 2 }}>
-              <Button onClick={handleInfo} disabled={!isFormComplete} variant="contained" size="large" sx={{ width: '100%', borderRadius: '8px', backgroundColor: 'var(--primary-default)' }}>
-                확인
+                <Button
+                  onClick={handleInfo}
+                  disabled={!isFormComplete}
+                  variant="contained"
+                  size="large"
+                  sx={{ width: '100%', borderRadius: '8px', backgroundColor: 'var(--primary-default)' }}
+                >
+                  확인
                 </Button>
               </Grid>
             </AccordionDetails>
