1d9fad44154a1cc626770deb1f18d8d83176a8a6
shinhm1 <shinhm1@naver.com>
2024-11-05 02:25:37 +0900

    fix: DetailBar 최상단에 고정되도록 수정

--- src/stories/DetailBar.jsx
+++ src/stories/DetailBar.jsx
@@ -4,13 +4,17 @@ import BackIcon from '../assets/icons/back.svg?react';
 import styled from 'styled-components';
 
 const Header = styled.div`
+  position: sticky;
+  top: 0;
+  right: 0;
+  z-index: 10;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 80px;
-  padding: 0 24px;
+  padding: 24px 20px;
   background: #fff;
   box-shadow: 0px 2px 4px 0px rgba(24, 119, 242, 0.08);
 `;
