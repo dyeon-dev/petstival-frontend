2d0f191aab4e5da511f45f9d3a5f1388219ded01
shinhm1 <shinhm1@naver.com>
2024-11-06 16:59:07 +0900

    fix: 펫스티벌 인증 완료 페이지 CSS 수정 완료

--- src/pages/QRCodeAuth/QRCodeAuthOkPage.jsx
+++ src/pages/QRCodeAuth/QRCodeAuthOkPage.jsx
@@ -3,32 +3,33 @@ import styles from './QRCodeAuthOkPage.module.css';
 import QRCodeOkLogo from '../../assets/icons/QR_ok_logo.svg?react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../stores/useAuthStore';
-const QRCodeAuthOkPage = () =>{
-    const user = useAuthStore();
-    const navigate = useNavigate();
-    const handleGoBack = () => {
-        navigate('/'); // This will navigate back to the previous page
-      };
-      useEffect(() =>{
-        if(!user.user){
-            navigate('/login');
-        }
+import ButtonLarge from '../../components/Common/Button/ButtonLarge';
 
-      },[user])
-      if (!user) return null;
-    return (
+const QRCodeAuthOkPage = () => {
+  const user = useAuthStore();
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate('/'); // This will navigate back to the previous page
+  };
+  useEffect(() => {
+    if (!user.user) {
+      navigate('/login');
+    }
+  }, [user]);
+  if (!user) return null;
 
-        <div className={styles.container}>
-            <div className={styles.logoLayout}>
-             <QRCodeOkLogo/>
-             <h1>참여 인증이 완료되었어요.</h1>
-             <p>참여 인증 뱃지를 발급했어요!<br/>펫스티벌 페이지에서 확인할 수 있어요.</p>
-            </div>
-        
-            <button className={styles.returnButton} onClick={handleGoBack}>
-                돌아가기
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <QRCodeOkLogo />
+      <h2 style={{ marginTop: '24px', marginBottom: '8px' }}>참여 인증이 완료되었어요.</h2>
+      <div style={{ fontSize: '16px', color: 'var(--gray-60)' }}>
+        참여 인증 뱃지를 발급했어요!
+        <br />
+        펫스티벌 페이지에서 확인할 수 있어요.
+      </div>
+
+      <ButtonLarge children="돌아가기" onClick={handleGoBack} />
+    </div>
+  );
+};
 export default QRCodeAuthOkPage;
